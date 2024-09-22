ff8031105fa2236b95504c1c8ce810ed7ad4325a
FrancoisMRib <fr.ribere@gmail.com>
2024-09-22 21:31:34 +0200

    Réfection App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
     <div>
         <h1>Portfolio de François Ribere</h1>
         <h2>Voici mon portfolio !!</h2>
@@ -11,6 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <p>en vrai c'est sympa et puis tant mieux surtout que je dois écrire du</p>
         <p>texte là</p>
     </div>
-  </React.StrictMode>,
-)
+  {/* </React.StrictMode>,
+) */}
 
