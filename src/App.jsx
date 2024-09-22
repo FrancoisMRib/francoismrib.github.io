0ac46644a316fefb16d7c28935e78211fcf25c37
FrancoisMRib <fr.ribere@gmail.com>
2024-09-22 19:58:00 +0200

    Ajout de App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+        <h1>Portfolio de François Ribere</h1>
+        <h2>Voici mon portfolio !!</h2>
+        <p>Je suis strès heureux que vous soyez venu sur mon portfolio parce que</p>
+        <p>en vrai c'est sympa et puis tant mieux surtout que je dois écrire du</p>
+        <p>texte là</p>
+    </div>
+  </React.StrictMode>,
+)
+
